c63570610a425c27c4c95ca3f3e709fbf77ccac0
Nathalia Pacheco <141690246+NathPach@users.noreply.github.com>
2023-09-02 20:41:41 -0300

    Aprovado ou Reprovado

--- 1º Semestre/Algoritmos e Linguagem de Programação/Questão 17.cs
+++ 1º Semestre/Algoritmos e Linguagem de Programação/Questão 17.cs
@@ -1,5 +1,5 @@
 {
-// 17. Escrever um programa fonte na Linguagem C# que efetue a leitura de quatro valores reais referentes a quatro notas escolares de um aluno e
+// 17. Escrever um programa na Linguagem C# que efetue a leitura de quatro valores reais referentes a quatro notas escolares de um aluno e
 // apresente mensagem informando que o aluno está aprovado caso o valor da média escolar seja maior ou igual a 5.
 // Caso o aluno não seja aprovado, apresente mensagem informando que o aluno encontra-se reprovado.
 // Ao final, além das mensagens, apresente o valor da média do aluno.
