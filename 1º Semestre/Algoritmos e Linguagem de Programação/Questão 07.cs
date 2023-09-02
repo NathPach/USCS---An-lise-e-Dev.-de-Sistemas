39347607301da8354a514ca7202a9d56287916a5
Nathalia Pacheco <141690246+NathPach@users.noreply.github.com>
2023-09-02 20:38:00 -0300

    Valor a ser pago: Cópias "xerox"

--- 1º Semestre/Algoritmos e Linguagem de Programação/Questão 07.cs
+++ 1º Semestre/Algoritmos e Linguagem de Programação/Questão 07.cs
@@ -1,5 +1,5 @@
 {
-// 7. Escrever um programa fonte na Linguagem C# que resolva o seguinte problema: uma cópia “xerox” custa R$ 0,50 por folha,
+// 7. Escrever um programa na Linguagem C# que resolva o seguinte problema: uma cópia “xerox” custa R$ 0,50 por folha,
 // mas acima de 200 folhas esse valor cai para R$ 0,30 por unidade. Dado o total de cópias, informe o valor a ser pago.
 
 int folhas;
