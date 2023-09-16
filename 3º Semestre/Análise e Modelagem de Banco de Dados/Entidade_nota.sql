7b422bb521b2e421774fc25d3f43f63f0ba93c42
Nathalia Pacheco <141690246+NathPach@users.noreply.github.com>
2023-09-16 19:45:32 -0300

    Nota

---
+++ 3º Semestre/Análise e Modelagem de Banco de Dados/Entidade_nota.sql
@@ -0,0 +1,8 @@
+CREATE TABLE nota(
+     chnota   int  IDENTITY(1,1) NOT NULL,
+     chaluno   int  NULL,
+     n1   decimal (10, 2) NULL,
+     n2   decimal (10, 2) NULL,
+     n3   decimal (10, 2) NULL
+) 
+GO
